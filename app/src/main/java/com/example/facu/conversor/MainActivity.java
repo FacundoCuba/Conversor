00061dd0a1c7038cb19487e6838453b686b38ede
FacundoCuba <facundogcuba@gmail.com>
2017-10-29 01:00:40 -0300

    Diseño inicial sin funcionalidad

--- app/src/main/java/com/example/facu/conversor/MainActivity.java
+++ app/src/main/java/com/example/facu/conversor/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.facu.conversor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner unidadesIniciales = findViewById(R.id.spUnidadInicial);
+        unidadesIniciales.setOnItemSelectedListener(this);
+        Spinner unidadesFinales = findViewById(R.id.spUnidadFinal);
+        unidadesFinales.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
